15a4b8c83de30d49e629fad6db2ae573f0370e35
chetana7483 <167082554+chetana7483@users.noreply.github.com>
2024-05-02 09:32:44 +0530

    Update and rename Biometric_Data_Management_System.py to Biometric_Data_Management_System_withoutFIleConcept.py

--- Biometric_Data_Management_System_withoutFIleConcept.py
+++ Biometric_Data_Management_System_withoutFIleConcept.py
@@ -1,16 +1,17 @@
 class BiometricData:
-    def __init__(self, name, age, DoB, nationality, id_number,biometric_type, visa_number):
+    def __init__(self, name, age, DoB, nationality, id_number, biometric_type, visa_number):
         self.name = name
         self.age = age
-        self.DoB= DoB
+        self.DoB = DoB
         self.nationality = nationality
         self.id_number = id_number
-        self.biometric_type= biometric_type
+        self.biometric_type = biometric_type
         self.visa_number = visa_number
 
 class BiometricDataManager:
     def __init__(self):
         self.data = {}
+        self.enroll_data = []
 
     def create_Data(self, Data):
         if Data.id_number not in self.data:
@@ -31,16 +32,16 @@ class BiometricDataManager:
             old_data = self.data[id_number]
             name = input("Enter new name (leave it blank to keep it as it is): ")
             age = input("Enter new age: ")
-            DoB = input("Enter new DoB(DD-MM-YY) :")
+            DoB = input("Enter new DoB(DD-MM-YY): ")
             nationality = input("Enter new nationality: ")
-            biometric_type= input("Enter new biometric_type: ")
+            biometric_type = input("Enter new biometric_type: ")
             visa_number = input("Enter new visa number: ")
 
             if name == "":
-                name = old_data.name 
+                name = old_data.name
 
             if age == "":
-                age = old_data.age 
+                age = old_data.age
             else:
                 age = int(age)
 
@@ -51,18 +52,16 @@ class BiometricDataManager:
                 nationality = old_data.nationality
 
             if biometric_type == "":
-                biometric_type = old_data.biometric_type 
-
+                biometric_type = old_data.biometric_type
 
             if visa_number == "":
                 visa_number = old_data.visa_number
 
-            new_Data = BiometricData(name, age, DoB, nationality, id_number,biometric_type, visa_number)
+            new_Data = BiometricData(name, age, DoB, nationality, id_number, biometric_type, visa_number)
             self.data[id_number] = new_Data
             print("Biometric data updated successfully.")
         else:
             print("id number not found in the database.")
-
 
     def verify_Data(self, id_number):
         if id_number in self.data:
@@ -71,43 +70,50 @@ class BiometricDataManager:
             print("Biometric Data not found in the database.")
 
     def display_all(self):
-        print("Biometric data for all Data:")
-        for id_number,Data in self.data.items():
-            print(f"ID Number: {Data.id_number}")
-            print(f"Name: {Data.name}")
-            print(f"Age: {Data.age}")
-            print(f"DoB: {Data.DoB}")
-            print(f"biometric_type: {Data.biometric_type}")
-            print(f"nationality: {Data.nationality}")
-            print(f"Visa Number: {Data.visa_number}")
-            print()
+        if self.data:
+            for id_number, Data in self.data.items():
+                print(f"ID Number: {Data.id_number}")
+                print(f"Name: {Data.name}")
+                print(f"Age: {Data.age}")
+                print(f"DoB: {Data.DoB}")
+                print(f"biometric_type: {Data.biometric_type}")
+                print(f"nationality: {Data.nationality}")
+                print(f"Visa Number: {Data.visa_number}")
+                print()
+        else:
+            print("There is no Data to be displayed")
+
+    def manage_enroll_data(self, enroll_data):
+        self.enroll_data.append(enroll_data)
+        print(f"Enroll data added for {enroll_data.name}")
+
 
 def main():
     manager = BiometricDataManager()
 
     while True:
-        print("\n\n**************************************************************")
-        print("\n~~~~~Menu~~~~~:")
+        print("\n\n***************************************************")
+        print("\n~~~~~~~~~~Menu:~~~~~~~~~")
         print("1. Create biometric data")
         print("2. Delete biometric data")
         print("3. Update biometric data")
         print("4. Verify biometric data")
         print("5. Display All")
-        print("6. Exit")
+        print("6. Manage Enroll Data")
+        print("7. Exit")
 
         choice = input("Enter your choice: ")
-        print("**************************************************************")
-
+        print("***********************************************")
 
         if choice == '1':
             id_number = input("Enter ID number: ")
             name = input("Enter name: ")
             age = int(input("Enter age: "))
-            DoB =input("Enter DoB(DD-MM-YY): ")
-            nationality= input("Enter nationality :")
+            DoB = input("Enter DoB(DD-MM-YY): ")
+            nationality = input("Enter nationality: ")
             biometric_type = input("Enter biometric_type: ")
             visa_number = input("Enter visa number: ")
-            Data = BiometricData(name, age, DoB, nationality, id_number,biometric_type, visa_number)
+            Data = BiometricData(name, age, DoB, nationality, id_number, biometric_type, visa_number)
             manager.create_Data(Data)
 
         elif choice == '2':
@@ -115,8 +121,8 @@ def main():
             manager.delete_Data(id_number)
 
         elif choice == '3':
-            id_number = input("Enter ID number you want to: ")
-            manager.update_Data( id_number)
+            id_number = input("Enter ID number you want to update: ")
+            manager.update_Data(id_number)
 
         elif choice == '4':
             id_number = input("Enter id number to verify: ")
@@ -126,6 +132,17 @@ def main():
             manager.display_all()
 
         elif choice == '6':
+            id_number=int(input("Enter id_number: "))
+            name = input("Enter name: ")
+            age = int(input("Enter age: "))
+            DoB = input("Enter DoB(DD-MM-YY): ")
+            nationality = input("Enter nationality: ")
+            biometric_type = input("Enter biometric_type: ")
+            visa_number = input("Enter visa number: ")
+            enroll_data = BiometricData(id_number,name, age, DoB, nationality, None, biometric_type, visa_number)
+            manager.manage_enroll_data(enroll_data)
+
+        elif choice == '7':
             print("Exiting...")
             break
 
